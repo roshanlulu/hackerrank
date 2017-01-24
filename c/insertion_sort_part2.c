9844e78aac441f7e9f8c7311b87ff2d3f33808ee
Roshan Lulu <roshanlulu@gmail.com>
2017-01-24 15:59:26 +1100

    Optimized insertion sort algorithm

--- c/insertion_sort_part2.c
+++ c/insertion_sort_part2.c
@@ -2,29 +2,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-
-void sort_element(int array_size, int * array){
-  int sortnum = array[array_size - 1];
-  bool arrayissorted = false;
-
-  for(int innerloopcnt = array_size - 1; innerloopcnt >= 0 && !arrayissorted; innerloopcnt--){
-    if(array[innerloopcnt - 1] > sortnum){
-      array[innerloopcnt] = array[innerloopcnt - 1];
-    }
-    else if((array[innerloopcnt - 1] <= sortnum) || (innerloopcnt == 0)){
-      array[innerloopcnt] = sortnum;
-      arrayissorted = true;
-    }
-  }
-}
-
 void insertion_sort(int array_size, int * array){
 
-  bool arrayissorted = false;
+  int sortnum;
 
   for (int loopcnt = 1; loopcnt < array_size; loopcnt++){
-    sort_element(loopcnt + 1, array);
+    sortnum = array[loopcnt];
+    int subarraysize = loopcnt;
+    while((subarraysize > 0) && (array[subarraysize - 1] > sortnum)){
+      array[subarraysize] =  array[subarraysize - 1];
+      subarraysize--;
+    }
+    array[subarraysize] = sortnum;
     for(int innerloopcnt = 0; innerloopcnt < array_size; innerloopcnt++){
       printf("%d ", array[innerloopcnt]);
     }
